b77137c323bfe1e50218ee3a6b17c0a0d8b09a65
Alexandre_maison <alexandre.prigent2910@gmail.com>
2025-02-02 19:27:06 +0100

    travail du dimanche

--- Script.js
+++ Script.js
@@ -1,15 +1,17 @@
-  function basculerElement(mon) {
+  function basculerElement(mon, bouton) {
     const element = document.getElementById(mon);
+    
+    bouton.classList.toggle("enfonce");
 
-    // Si l'élément est masqué, l'afficher, sinon le masquer
     if (element.style.display === "none") {
         element.style.display = "flex" ;
+        
     } else {
         element.style.display = "none";
+        
     }
+    
 }
-const elementCache1 = document.getElementById('cv');
-const elementCache2 = document.getElementById('experiences');
-const elementCache3 = document.getElementById('diplomes');
-const elementCache4 = document.getElementById('bonus');
+
+
 
